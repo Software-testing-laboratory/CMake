607875e26796adc1e7733903c2b239c857c0d187
shivapbhusal <civabhusal@gmail.com>
2017-12-10 14:16:29 -0500

    add STEP_2

---
+++ STEP_2/Calculator.hpp
@@ -0,0 +1,16 @@
+/* This is a simple header file for a calcualtor program. 
+*/
+
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator {
+    public:
+	int sum(int x, int y);
+    int subs( int x, int y); 
+
+}; 
+
+
+#endif 
+
